595ccb00b89b1bb4fc539de64f38a68c99ec57f9
wyattpfeil <wyatt.pfeil@gmail.com>
2019-02-12 15:50:54 -0500

    Update Main.java

--- BPick/src/me/wyatt/BPick/Main.java
+++ BPick/src/me/wyatt/BPick/Main.java
@@ -3,10 +3,12 @@ package me.wyatt.BPick;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import me.wyatt.BPick.commands.TestCommand;
+import me.wyatt.BPick.commands.ChatPlayer;
 
 public class Main extends JavaPlugin{
 	@Override
 	public void onEnable() {
 		new TestCommand(this);
+		new ChatPlayer(this);
 	}
 }
